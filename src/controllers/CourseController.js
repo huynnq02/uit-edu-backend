067b9b4ae0cb4c178f642a9134564726642cb772
Huy Nguyen <huyy.802@gmail.com>
2023-12-25 00:56:01 +0700

    chore: refactor the code structure

--- src/controllers/CourseController.js
+++ src/controllers/CourseController.js
@@ -1,15 +1,5 @@
 import Course from "../models/course.js";
-import { v2 as cloudinary } from "cloudinary";
-import dotenv from "dotenv"; // Import dotenv for dynamic environment variables
-
-dotenv.config();
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
+import cloudinary from "../lib/cloudinary.js";
 const CourseController = {
   // Create a new course
   createCourse: async (req, res) => {
